23bd4001047f6861f2cb657c1abc5ec15fc9cd0e
carlinjshaw <carlinjshaw@gmail.com>
2021-06-07 17:22:13 -0400

    put together the two APIs

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,34 +19,55 @@ var formSubmitHandler = function(event) {
 userFormEl.addEventListener("submit", formSubmitHandler);
 
 
-//old shit
 var cityLocation = function (city) {
-    // var city = "new york"
-    console.log("function has started")
+    // console.log("function has started")
     var firstApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + 
     '&exclude=temp&appid=60f419afbafe35f58528b7ec4e85d06a';
     fetch(firstApiUrl)
     .then(function(response) {
         if (response.ok) {
-         console.log(response);
+        //  console.log(response);
          response.json().then(function(data){
-console.log(data);
-
-        });
-    } else {
-
-    }
+             var latitude = data.coord.lat;
+console.log(latitude);
+var longitude = data.coord.lon;
+console.log(longitude);
+var coordinate = "lat=" + latitude + "&lon=" + longitude;
+console.log(coordinate)
+forecast(coordinate)
+// var coordString = JSON.stringify(coord)
+// console.log(coordString);
+// forecast(coordString)
+});
+} else {
+    alert('please enter a city name')
+}
 });
 //    .catch(function(error){
-// alert('unable to connect')
-//    })            
+    // alert('unable to connect')
+    //    })            
 };
 
 
 
+var forecast = function (location) {
+    // location = "lat=33.44&lon=-94.04"
+    console.log("the function has started")
+    var secondAPiUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + location + 
+    '&exclude=hourly,minutely,current&appid=60f419afbafe35f58528b7ec4e85d06a';
+    fetch(secondAPiUrl)
+    .then(function(response) {
+        if (response.ok) {
+console.log(response);
+response.json().then(function(data){
+    console.log(data)
+})        
+}
+    })
+}
+forecast()
 
-
-
+// lat=33.44&lon=-94.04
 // 7 day forecast
 // function start () {
 //     console.log("function has started")

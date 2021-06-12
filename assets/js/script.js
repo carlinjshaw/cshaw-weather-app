626c118ac4ec4d00e145eeb8ddfafd51fb7e15cd
carlinjshaw <carlinjshaw@gmail.com>
2021-06-11 21:15:16 -0400

    got all the data for weekly forecase rendered except pics

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,25 +59,80 @@ var forecast = function (location) {
     if (response.ok) {
       response.json().then(function (data) {
           console.log(data)
+//variables for the current day
         var currentTemp = JSON.stringify(data.current.temp);
-        console.log(currentTemp)
-        var currentWind = JSON.stringify(data.current.wind_speed)
-        // console.log(data.current.wind_speed);
-        // console.log(data.current.humidity);
-        // console.log(data.current.uvi)
-        // console.log(data.current.weather[0].icon)
-        createContent(currentTemp, currentWind)
+        var currentWind = JSON.stringify(data.current.wind_speed);
+        var currentHum = JSON.stringify(data.current.humidity);
+        var currentUvi = JSON.stringify(data.current.uvi);
+//variables for the first day of forecast
+        var daytwoTemp = "High: " + JSON.stringify(data.daily[0].temp.max) + 
+            " ℉ Low: " + JSON.stringify(data.daily[0].temp.min) + " ℉";
+        var daytwoWind = JSON.stringify(data.daily[0].wind_speed);
+        var daytwoHum = JSON.stringify(data.daily[0].humidity);
+//variable for the second day of forecast
+        var daythreeTemp = "High: " + JSON.stringify(data.daily[1].temp.max) +
+             " ℉ Low: " + JSON.stringify(data.daily[1].temp.min) + " ℉";
+        var daythreeWind = JSON.stringify(data.daily[1].wind_speed);
+        var daythreeHum = JSON.stringify(data.daily[1].humidity);
+//variable for the third day of forecast
+        var dayfourTemp = "High: " + JSON.stringify(data.daily[2].temp.max) +
+             " ℉ Low: " + JSON.stringify(data.daily[2].temp.min) + " ℉";
+        var dayfourWind = JSON.stringify(data.daily[2].wind_speed);
+        var dayfourHum = JSON.stringify(data.daily[2].humidity);
+//variable for the third day of forecast
+        var dayfiveTemp = "High: " + JSON.stringify(data.daily[3].temp.max) +
+            " ℉ Low: " + JSON.stringify(data.daily[3].temp.min) + " ℉";
+        var dayfiveWind = JSON.stringify(data.daily[3].wind_speed);
+        var dayfiveHum = JSON.stringify(data.daily[3].humidity);
+//variable for the fourth day of forecast
+        var daysixTemp = "High: " + JSON.stringify(data.daily[4].temp.max) +
+            " ℉ Low: " + JSON.stringify(data.daily[4].temp.min) + " ℉";
+        var daysixWind = JSON.stringify(data.daily[4].wind_speed);
+        var daysixHum = JSON.stringify(data.daily[4].humidity);
+        
+        createContent(currentTemp, currentWind, currentHum, currentUvi, 
+            daytwoTemp, daytwoWind, daytwoHum,
+            daythreeTemp, daythreeWind, daythreeHum,
+            dayfourTemp, dayfourWind, dayfourHum,
+            dayfiveTemp, dayfiveWind, dayfiveHum,
+            daysixTemp, daysixWind, daysixHum)
       });
     }
   });
 };
 
-var createContent = function (temp, wind) {
-//   var tempEl = document.getElementById("current-data");
-//   tempEl.textContent(temp);
+var createContent = function (temp, wind, humidity, uvi, 
+    d2Temp,d2Wind, d2Hum,
+    d3Temp,d3Wind, d3Hum,
+    d4Temp, d4Wind, d4Hum,
+    d5Temp, d5Wind, d5Hum,
+    d6Temp, d6Wind, d6Hum) {
+
 console.log(temp)
   $("#current-data").text(temp + " ℉");
   $("#current-wind").text(wind + " MPH");
+  $("#current-humidity").text(humidity + " %");
+  $("#current-uvi").text(uvi);
+
+  $("#daytwo-temp").text(d2Temp);
+  $("#daytwo-wind").text(d2Wind + " MPH");
+  $("#daytwo-humidity").text(d2Hum + " %");
+
+  $("#daythree-temp").text(d3Temp);
+  $("#daythree-wind").text(d3Wind + " MPH");
+  $("#daythree-humidity").text(d3Hum + " %");
+
+  $("#dayfour-temp").text(d4Temp);
+  $("#dayfour-wind").text(d4Wind + " MPH");
+  $("#dayfour-humidity").text(d4Hum + " %")
+  
+  $("#dayfive-temp").text(d5Temp);
+  $("#dayfive-wind").text(d5Wind + " MPH");
+  $("#dayfive-humidity").text(d5Hum + " %")
+  
+  $("#daysix-temp").text(d6Temp);
+  $("#daysix-wind").text(d6Wind + " MPH");
+  $("#daysix-humidity").text(d6Hum + " %");
 };
 
 
